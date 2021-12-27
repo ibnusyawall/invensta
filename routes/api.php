e649e444dd961e9418355bab27f38512e7baccbe
ibnusyawall <website.isas.24@gmail.com>
2021-12-27 11:07:52 +0700

    add: routes api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'v1'], function() {
+    Route::post('auth/login', 'API\AuthController@login');
+    Route::post('auth/pegawai/login', 'API\AuthController@loginPegawai');
+});
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function() {
+    Route::resource('jenis', 'API\JenisController');
+    Route::resource('ruang', 'API\RuangController');
+    Route::resource('level', 'API\LevelController');
+    Route::resource('petugas', 'API\PetugasController');
+    Route::resource('inventaris', 'API\InventarisController');
+    Route::resource('pegawai', 'API\PegawaiController');
+    Route::resource('peminjaman', 'API\PeminjamanController');
+    Route::resource('detail-pinjam', 'API\DetailPinjamController');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
