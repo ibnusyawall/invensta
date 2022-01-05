4a6c3210a89831378f91632612d17484b55f7ecd
ibnusyawall <website.isas.24@gmail.com>
2022-01-05 10:26:22 +0700

    modified: store auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,15 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+import { ValidationProvider, extend } from 'vee-validate'
+import * as rules  from 'vee-validate/dist/rules'
+
 import router from './router'
-import store from './store';
+import store from './store'
 import App from './components/App.vue'
-import { setHeaderToken } from './components/utils/auth';
+import { setHeaderToken } from './utils/auth'
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -22,14 +27,41 @@ const token = localStorage.getItem('token')
 if (token) {
     setHeaderToken(token)
 }
-store.dispatch('get_user', token)
-    .then(() => {
-        new Vue({
-            el: '#app',
-            components: {
-                App
-            },
-            router,
-            store
-        });
-    })
+
+Object.keys(rules).forEach(rule => {
+    extend(rule, rules[rule])
+})
+
+/**
+ * @extend custom required validate form
+ **/
+
+// extend('required', {
+//     ...required,
+//     message: 'The {_field_} is required.'
+// })
+
+/**
+ * @extend custom min length validate form
+ **/
+
+// extend('min', {
+//     validate(value, { length }) {
+//         return value.length >= length
+//     },
+//     params: ['length'],
+//     message: 'The {_field_} field must have at least {length} characters.'
+// })
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: {
+        App,
+        ValidationProvider
+    },
+    data: () => ({
+        value: ''
+    }),
+})
--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,16 +1,36 @@
 import axios from "axios"
-import { reject } from "lodash"
-import { setHeaderToken, removeHeaderToken } from "../components/utils/auth"
+import { setHeaderToken, removeHeaderToken } from "../utils/auth"
 
 export default {
+    strict: true,
     state: {
         user: null,
-        isLoggedIn: false
+        isLoggedIn: false,
+        brand: 'INVENSTA',
+        errors: [],
+        isError: false,
     },
     mutations: {
         set_user(state, data) {
             state.user = data
             state.isLoggedIn = true
+        },
+        set_example(state, data = {}) {
+            state.example = { ...data }
+        },
+        set_errors(state, e) {
+            state.errors.push(e)
+            state.isError = true
+        },
+        close_errors(state) {
+            state.errors = []
+            state.isError = false
+        },
+        add_count(state) {
+            state.count += 1
+        },
+        reset_count(state) {
+            state.count = 1
         },
         reset_user(state) {
             state.user = null
@@ -21,22 +41,51 @@ export default {
         user(state) {
             return state.user
         },
+        example(state) {
+            return state.example
+        },
         isLoggedIn(state) {
             return state.isLoggedIn
-        }
+        },
+        get_state(state) {
+            return state
+        },
+        get_count(state) {
+            return state.count
+        },
     },
     actions: {
-        login({ dispatch, commit }, type) {
+        get_example({ dispatch, commit }, v) {
+            dispatch('_getsample', v)
+            //commit('set_example', data)
+        },
+        async _getsample({ commit }, v) {
+            let data = v || { a: 1, b: 2 }
+            commit('set_example', data)
+        },
+        close({ commit }) {
+            commit('close_errors')
+        },
+        add_count({ commit }) {
+            commit('add_count')
+        },
+        reset_count({ commit }) {
+            commit('reset_count')
+        },
+        login({ dispatch, commit, getters }, value, type) {
             return new Promise((resolve, reject) => {
                 switch (type) {
                     case 'pegawai':
-                        axios.post('/api/v1/auth/pegawai/login')
+                        axios.post('/api/v1/auth/pegawai/login', value)
                             .then(response => {
                                 var token = response?.data.token
+                                var user = response?.data.user
+
                                 localStorage.setItem('token', token)
                                 setHeaderToken(token)
 
                                 dispatch('get_user')
+                                commit('set_user', user)
                                 resolve(response)
                             }).catch(e => {
                                 commit('reset_user')
@@ -45,16 +94,22 @@ export default {
                             })
                         break
                     default:
-                        axios.post('/api/v1/auth/login')
+                        axios.post('/api/v1/auth/login', value)
                             .then(response => {
-                                var token = response?.data.token
+                                var token = response?.data?.token
+                                var user = response?.data?.user
+
                                 localStorage.setItem('token', token)
                                 setHeaderToken(token)
 
                                 dispatch('get_user')
+                                commit('set_user', user)
                                 resolve(response)
                             }).catch(e => {
                                 commit('reset_user')
+                                commit('set_errors', 'username atau password salah.')
+                                let _e = getters.get_state
+                                console.log(_e)
                                 localStorage.removeItem('token')
                                 reject(e)
                             })
