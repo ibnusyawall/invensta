334f1ae91e4c7e7814972006597db89f9cbd65dd
ibnusyawall <website.isas.24@gmail.com>
2022-01-17 18:08:57 -0800

    modified: app blade template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,14 +10,14 @@
   <link rel="stylesheet" href="https://raw.githubusercontent.com/lcdsantos/jQuery-Selectric/master/public/selectric.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
   <!-- CSS Libraries -->
-  <!-- Template CSS -->
+  <!-- Template CSS --><!-- 
   <link rel="stylesheet" href="{{ asset('stisla/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('stisla/css/components.css')}}">
-  <link rel="stylesheet" href="{{ asset('stisla/css/custom.css') }}">
+  <link rel="stylesheet" href="{{ asset('stisla/css/custom.css') }}"> -->
   <link href="{{ asset('css/app.css') }}">
 </head>
 
-<body class="sidebar-gone">
+<body>
   <div id="app">
       <app></app>
   </div>
@@ -26,9 +26,8 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="https://raw.githubusercontent.com/lcdsantos/jQuery-Selectric/master/public/jquery.selectric.min.js">
-  <script src="{{ asset('stisla/js/stisla.js') }}"></script>
-  <script src="{{ asset('stisla/js/scripts.js') }}"></script>
+  <script src="https://raw.githubusercontent.com/lcdsantos/jQuery-Selectric/master/public/jquery.selectric.min.js"></script>
+  
   <script src="//cdn.jsdelivr.net/npm/eruda"></script>
   <script>eruda.init();</script>
   
